60931ec4f9efc4b9a3a14672f72814b1d0d05dbe
Rajatcoder066 <88333677+Rajatcoder066@users.noreply.github.com>
2024-04-30 01:02:20 +0530

    dfds

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -2,20 +2,20 @@
 
 // Development Connection
 // Server name or IP Address
-$host = "";
+$host = "roundhouse.proxy.rlwy.net";
 
 // MySQL Username
 $user = "root";
 
 // MySQL Password
-$pass = "";
+$pass = "pzOkYUboNOcgdOuVjqiFXbssuLZiEZsq";
 
 // Default Database name
-$db = "news";
+$db = "railway";
 
 // Creating a connection to the DataBase
 #$conn = new mysqli($host, $user, $pass, $db);  
-$con = mysqli_connect($host,$user,$pass,$db);
+$con = mysqli_connect($host,$user,$pass,$db,27857);
 
 /* Deployment Connection
 
